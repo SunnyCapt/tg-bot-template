cd20d732e7d3c5b068f1e34c41305178d0d130f0
SunnyCapt <42912268+SunnyCapt@users.noreply.github.com>
2019-11-13 00:59:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# in ~/path/to/moe-ctf-bot
+# in ~/path/to/tg-bot-template
 apt-get install git tor python3-virtualenv
 virtualenv .venv --no-site-packages --python=python3
 source .venv/bin/activate
